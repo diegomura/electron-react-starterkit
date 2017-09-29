6c818139feae3e2da04b54a96bcdd56b17bfded1
Diego Muracciole <diego.muracciole@moove-it.com>
2017-09-29 16:14:36 -0300

    Add packaging script

--- src/main.js
+++ src/main.js
@@ -1,32 +1,40 @@
-import { app, BrowserWindow } from 'electron';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow = null;
+const path = require('path');
+const url = require('url');
 
-app.on('window-all-closed', () => {
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL(`http://localhost:8080/src`);
+  } else {
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, '../dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-
-app.on('ready', async () => {
-  mainWindow = new BrowserWindow({
-    show: false,
-    width: 510,
-    height: 470
-  });
-
-  mainWindow.loadURL(`http://localhost:8080/`);
-
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    mainWindow.show();
-    mainWindow.focus();
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const childProcess = require('child_process');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  devtool: 'inline-source-map',
   // target: 'electron-renderer',
   entry: './index.js',
   output: {
@@ -13,6 +11,7 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
@@ -24,32 +23,18 @@ module.exports = {
       },
     ]
   },
-
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      inject: 'body',
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    }),
-    new webpack.LoaderOptionsPlugin({ debug: true }),
   ],
-  node: {
-    __dirname: false,
-    __filename: false
-  },
   devServer: {
     setup() {
       console.log('Staring Main Process...');
       childProcess.spawn(
         'npm',
-        ['run', 'start']
+        ['run', 'start'],
+        { shell: true, env: process.env, stdio: 'inherit' }
       )
       .on('close', code => process.exit(code))
       .on('error', spawnError => console.error(spawnError));
